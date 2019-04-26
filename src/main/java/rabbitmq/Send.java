99b6dee700b999b2d7604a8ff216dacb3d470672
fansy <fansy1990@foxmail.com>
2019-04-26 18:05:05 +0800

    JavaScript绑定client，后台代码Send发送消息 确认前台JavaScript可以接收到消息；

--- src/main/java/rabbitmq/Send.java
+++ src/main/java/rabbitmq/Send.java
@@ -1,11 +1,10 @@
 package rabbitmq;
 
-import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
 
 import java.io.IOException;
-import java.util.Random;
 import java.util.concurrent.TimeoutException;
 
 /**
@@ -14,7 +13,8 @@ import java.util.concurrent.TimeoutException;
  * date : 2019/4/21 AM11:16.
  */
 public class Send {
-    private final static String QUEUE_NAME = "hello";
+//    private final static String QUEUE_NAME = "hello";
+    private final static String QUEUE_NAME = "mqtt-subscription-myclientid_qos1";
 
     public static void main(String[] args) throws IOException, TimeoutException {
         ConnectionFactory factory = new ConnectionFactory();
@@ -22,13 +22,14 @@ public class Send {
         factory.setUsername("fansy");
         factory.setPassword("fansy");
 
-        int sleepTime = 30 ;
+        int sleepTime = 3000 ;
 
-        int msgSize = 10000;
+        int msgSize = 10;
 
         try (Connection connection = factory.newConnection();
              Channel channel = connection.createChannel()) {
-            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+//            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            channel.queueDeclare(QUEUE_NAME, true, false, true, null);
             String message = "Hello World!";
             String realMessage = null;
             long realSleepTime = 0;
--- src/main/resources/static/demo.js
+++ src/main/resources/static/demo.js
@@ -1,1 +1,31 @@
 console.log("Hello Demo");
+
+var wsbroker = "localhost";  // mqtt websocket enabled broker
+var wsport = 15675; // port for above
+var client = new Paho.MQTT.Client(wsbroker, wsport, "/ws",
+    "myclientid_");
+console.info(client)
+client.onConnectionLost = function (responseObject) {
+    console.info("CONNECTION LOST - " + responseObject.errorMessage);
+};
+
+client.onMessageArrived = function (message) {
+    console.info("RECEIVE ON " + message.destinationName + " PAYLOAD " + message.payloadString);
+};
+
+var options = {
+    timeout: 3,
+    keepAliveInterval: 30,
+    onSuccess: function () {
+        console.info("CONNECTION SUCCESS");
+        client.subscribe('hello', {qos: 1});
+    },
+    onFailure: function (message) {
+        console.info("CONNECTION FAILURE - " + message.errorMessage);
+    }
+};
+if (location.protocol == "https:") {
+    options.useSSL = true;
+}
+console.info("CONNECT TO " + wsbroker + ":" + wsport);
+client.connect(options);
